a6332b087fc6c17795a41b8597ab4e314a8e0de9
PracticalEscapement <69914892+PracticalEscapement@users.noreply.github.com>
2021-11-03 23:52:52 -0400

    Signup form (#2)
    * first last name addition to signup form
    
    * correction to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'home#index'
   get 'home/my_page', to: 'home#my_page', as: :my_page
 end
