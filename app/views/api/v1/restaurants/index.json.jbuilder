48e6a787f3af00fb09f87c8ca102dc261052dbfb
Michael Moradi <themoradis1@gmail.com>
2022-03-09 11:51:23 -0500

    current_users

---
+++ app/views/api/v1/restaurants/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.restaurants @restaurants, partial: 'api/v1/restaurants/restaurant', as: :restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'home#index'
+
 
   resources :restaurants, :posts
 
-  resources :restaurants do
-    resources :posts
-  end
+  #resources :restaurants do
+  #  resources :posts
+  #end
 
-  resources :restaurants do
-    resources :addresses
-  end
+  #resources :restaurants do
+  #  resources :addresses
+  #end
+
+  #delete '/restaurants/:restaurant_id/addresses/:id', to: 'addresses#destroy', as: :destroy_restaurant_address
 
   # Api Routes
   namespace :api do
-    namespace :v1 do
+    namespace :v1 do 
+      devise_for :users, controllers: {
+        registrations: 'api/v1/users/registrations',
+        sessions: 'api/v1/users/sessions',
+      }
+      resources :current_user, only: [:index]
       resources :restaurants, :posts, only: [:index, :show, :create, :destroy] 
+
     end
   end
-  
-  delete '/restaurants/:restaurant_id/addresses/:id', to: 'addresses#destroy', as: :destroy_restaurant_address
 
 end
