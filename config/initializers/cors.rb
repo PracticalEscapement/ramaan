7c019458d12fae67569792127b42aadce38db1ce
Michael Moradi <themoradis1@gmail.com>
2022-05-05 21:48:41 -0400

    cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://the-ramen-critique.herokuapp.com', 'localhost:3000']
+    origins ['https://the-ramen-critique.herokuapp.com', 'http://localhost:3000']
     resource '*',
       headers: :any,
       methods: [:get, :post, :delete, :create],
